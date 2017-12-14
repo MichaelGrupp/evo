8c03327a0c9535e19ab5e2d2568547b9012e7ddf
Michael Grupp <grupp@magazino.eu>
2017-12-14 16:32:31 +0100

    minor: no 'set -e' in latex_demo.sh

--- test/demos/latex_demo.sh
+++ test/demos/latex_demo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e  # exit on error
+# set -e  # exit on error
 
 # printf "\033c" resets the output
 function log { printf "\033c"; echo -e "\033[32m[$BASH_SOURCE] $1\033[0m"; }

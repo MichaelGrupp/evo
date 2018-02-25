6ccc62e41267042acf39e1e585dfdf6b74f79e30
Michael Grupp <grupp@magazino.eu>
2018-02-25 20:40:27 +0100

    Add smoke test for evo_res (#22)

--- test/ape_rpe_smoke_test.py
+++ test/ape_rpe_smoke_test.py
@@ -10,23 +10,21 @@ tmp_dir = "tmp"
 cfg_dir = "cfg/ape_rpe"
 here = os.path.dirname(os.path.abspath(__file__))
 
+# always run in script location
+os.chdir(here)
+
 metrics = ["evo_ape", "evo_rpe"]
 
-data = ["euroc data/V102_groundtruth.csv data/V102.txt",
-        "kitti data/KITTI_00_gt.txt data/KITTI_00_ORB.txt",
-        "tum data/fr2_desk_groundtruth.txt data/fr2_desk_ORB.txt"]
+data = [
+  "euroc data/V102_groundtruth.csv data/V102.txt",
+  "kitti data/KITTI_00_gt.txt data/KITTI_00_ORB.txt",
+  "tum data/fr2_desk_groundtruth.txt data/fr2_desk_ORB.txt"
+]
 try:
   import rosbag
   data.append("bag data/ROS_example.bag groundtruth S-PTAM")
 except:
   pass
-
-# always run in script location
-abspath = os.path.abspath(__file__)
-dirname = os.path.dirname(abspath)
-os.chdir(dirname)
-
-print("executing in {}".format(here))
 
 try:
   for m in metrics:

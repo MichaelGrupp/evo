b08741143241f3a2218d67fbde49a2f416dd0da3
Michael Grupp <michael.grupp@tum.de>
2018-03-18 20:06:47 +0100

    Add license header to log.py

--- evo/tools/log.py
+++ evo/tools/log.py
@@ -1,3 +1,24 @@
+# -*- coding: UTF8 -*-
+"""
+utilities for the configuration of the package's loggers
+author: Michael Grupp
+
+This file is part of evo (github.com/MichaelGrupp/evo).
+
+evo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+evo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with evo.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import logging
 import os
 import sys

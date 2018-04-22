215c3b1c641c55e93e18225363c809c76a1a23e2
Michael Grupp <michael.grupp@tum.de>
2018-04-22 15:18:56 +0200

    Adapt .travis.yml

---
+++ .ci/mac-os_before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# source: https://pythonhosted.org/CodeChat/.travis.yml.html
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    brew install openssl readline
+    brew outdated pyenv || brew upgrade pyenv
+
+    brew install pyenv-virtualenv
+    pyenv install $PYTHON
+
+    export PYENV_VERSION=$PYTHON
+    export PATH="/Users/travis/.pyenv/shims:${PATH}"
+    pyenv-virtualenv venv
+    source venv/bin/activate
+fi

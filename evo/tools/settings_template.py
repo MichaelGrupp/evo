fb24938826f7f5a16a0fc4a37f02972b0d9c365b
Michael Grupp <grupp@magazino.eu>
2019-01-15 13:13:46 +0100

    Sort default settings dictionary by name.

--- evo/tools/settings_template.py
+++ evo/tools/settings_template.py
@@ -32,25 +32,25 @@ def get_default_plot_backend():
 # default settings with documentation
 # yapf: disable
 DEFAULT_SETTINGS_DICT_DOC = {
-    "plot_xyz_realistic": (
-        True,
-        "Equal axes ratio in 'xyz' plot mode for realistic trajectory plots."
+    "logfile_enabled": (
+        False,
+        "Whether to write a logfile to the home folder."
+    ),
+    "logging_format": (
+        "%(message)s",
+        "Format string for the logging module (console only)."
     ),
     "plot_backend": (
         get_default_plot_backend(),
         "matplotlib backend - default: 'Qt{4, 5}Agg' (if PyQt is installed) or 'TkAgg'."
     ),
-    "plot_linewidth": (
-        1.5,
-        "Line width value supported by matplotlib."
+    "plot_export_format": (
+        "pdf",
+        "File format supported by matplotlib for exporting plots."
     ),
-    "plot_usetex": (
-        False,
-        "Use the LaTeX renderer configured in plot_texsystem for plots.",
-    ),
-    "plot_texsystem": (
-        "pdflatex",
-        "'xelatex', 'lualatex' or 'pdflatex', see: https://matplotlib.org/users/pgf.html",
+    "plot_figsize": (
+        [6, 6],
+        "The default size of one (sub)plot figure (width, height)."
     ),
     "plot_fontfamily": (
         "sans-serif",
@@ -60,23 +60,6 @@ DEFAULT_SETTINGS_DICT_DOC = {
         1.0,
         "Font scale value, see: https://seaborn.pydata.org/generated/seaborn.set.html"
     ),
-    "plot_split": (
-        False,
-        "Show / save each figure separately instead of a collection."
-    ),
-    "plot_figsize": (
-        [6, 6],
-        "The default size of one (sub)plot figure (width, height)."
-    ),
-    "plot_trajectory_cmap": (
-        "jet",
-        "matplotlib color map used for mapping values on a trajectory.",
-    ),
-    "plot_multi_cmap": (
-        "none",
-        "Color map for coloring plots from multiple data sources.\n"
-        + "'none' will use the default color palette, see plot_seaborn_palette."
-    ),
     "plot_invert_xaxis": (
         False,
         "Invert the x-axis of plots."
@@ -85,19 +68,14 @@ DEFAULT_SETTINGS_DICT_DOC = {
         False,
         "Invert the y-axis of plots."
     ),
-    "plot_seaborn_style": (
-        "darkgrid",
-        "Defines the plot background/grid.\n"
-        + "Options: 'whitegrid', 'darkgrid', 'white' or 'dark'."
+    "plot_linewidth": (
+        1.5,
+        "Line width value supported by matplotlib."
     ),
-    "plot_seaborn_palette": (
-        "deep6",
-        "Default color palette of seaborn. Can also be a list of colors.\n"
-        + "See: https://seaborn.pydata.org/generated/seaborn.color_palette.html"
-    ),
-    "plot_export_format": (
-        "pdf",
-        "File format supported by matplotlib for exporting plots."
+    "plot_multi_cmap": (
+        "none",
+        "Color map for coloring plots from multiple data sources.\n"
+        + "'none' will use the default color palette, see plot_seaborn_palette."
     ),
     "plot_reference_alpha": (
         0.5,
@@ -111,30 +89,52 @@ DEFAULT_SETTINGS_DICT_DOC = {
         "--",
         "matplotlib linestyle of reference trajectories in plots."
     ),
-    "table_export_format": (
-        "csv",
-        "Format for exporting tables, e.g. 'csv', 'excel', 'latex', 'json'...",
+    "plot_seaborn_palette": (
+        "deep6",
+        "Default color palette of seaborn. Can also be a list of colors.\n"
+        + "See: https://seaborn.pydata.org/generated/seaborn.color_palette.html"
     ),
-    "table_export_data": (
-        "stats",
-        "Which data to export: 'info', 'stats' or 'error_array'.",
+    "plot_seaborn_style": (
+        "darkgrid",
+        "Defines the plot background/grid.\n"
+        + "Options: 'whitegrid', 'darkgrid', 'white' or 'dark'."
     ),
-    "table_export_transpose": (
+    "plot_split": (
+        False,
+        "Show / save each figure separately instead of a collection."
+    ),
+    "plot_texsystem": (
+        "pdflatex",
+        "'xelatex', 'lualatex' or 'pdflatex', see: https://matplotlib.org/users/pgf.html",
+    ),
+    "plot_trajectory_cmap": (
+        "jet",
+        "matplotlib color map used for mapping values on a trajectory.",
+    ),
+    "plot_usetex": (
+        False,
+        "Use the LaTeX renderer configured in plot_texsystem for plots.",
+    ),
+    "plot_xyz_realistic": (
         True,
-        "Transpose tables for export."
+        "Equal axes ratio in 'xyz' plot mode for realistic trajectory plots."
     ),
     "save_traj_in_zip": (
         False,
         "Store backup trajectories in result zip files (increases size)."
     ),
-    "logging_format": (
-        "%(message)s",
-        "Format string for the logging module (console only)."
+    "table_export_data": (
+        "stats",
+        "Which data to export: 'info', 'stats' or 'error_array'.",
     ),
-    "logfile_enabled": (
-        False,
-        "Whether to write a logfile to the home folder."
-    )
+    "table_export_format": (
+        "csv",
+        "Format for exporting tables, e.g. 'csv', 'excel', 'latex', 'json'...",
+    ),
+    "table_export_transpose": (
+        True,
+        "Transpose tables for export."
+    ),
 }
 # yapf: enable
 

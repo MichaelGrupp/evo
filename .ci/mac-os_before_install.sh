b8132392ecfea02bc871a875843e748606ef0098
Michael Grupp <grupp@magazino.eu>
2019-01-30 10:45:01 +0100

    Test homebrew readline path export

--- .ci/mac-os_before_install.sh
+++ .ci/mac-os_before_install.sh
@@ -7,9 +7,14 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   brew install openssl readline
   brew outdated pyenv || brew upgrade pyenv
 
+  # As recommended by Homebrew's "keg-only" warning.
+  export LDFLAGS="-L /usr/local/opt/readline/lib"
+  export CPPFLAGS="-I /usr/local/opt/readline/include"
+  export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+
   brew install pyenv-virtualenv
   eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
+  eval "$(pyenv virtualenv-init -)"  
   pyenv install $PYTHON
 
   export PYENV_VERSION=$PYTHON

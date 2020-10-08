6214986e04aa7c6799a4fc7b58c7bf5bc13266c8
Michael Grupp <grupp@magazino.eu>
2020-10-08 19:35:08 +0200

    adjust classifiers

--- setup.py
+++ setup.py
@@ -116,9 +116,10 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )

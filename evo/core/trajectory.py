a4cb04fefee80ff210c3e5961982bae3e65c2a69
Michael Grupp <grupp@magazino.eu>
2022-01-03 11:46:05 +0100

    fix format

--- evo/core/trajectory.py
+++ evo/core/trajectory.py
@@ -42,10 +42,10 @@ class PosePath3D(object):
     also: base class for real trajectory
     """
     def __init__(
-        self, positions_xyz: typing.Optional[np.ndarray] = None,
-        orientations_quat_wxyz: typing.Optional[np.ndarray] = None,
-        poses_se3: typing.Optional[typing.Sequence[np.ndarray]] = None,
-        meta: typing.Optional[dict] = None):
+            self, positions_xyz: typing.Optional[np.ndarray] = None,
+            orientations_quat_wxyz: typing.Optional[np.ndarray] = None,
+            poses_se3: typing.Optional[typing.Sequence[np.ndarray]] = None,
+            meta: typing.Optional[dict] = None):
         """
         :param positions_xyz: nx3 list of x,y,z positions
         :param orientations_quat_wxyz: nx4 list of quaternions (w,x,y,z format)
@@ -298,11 +298,11 @@ class PoseTrajectory3D(PosePath3D, object):
     a PosePath with temporal information
     """
     def __init__(
-        self, positions_xyz: typing.Optional[np.ndarray] = None,
-        orientations_quat_wxyz: typing.Optional[np.ndarray] = None,
-        timestamps: typing.Optional[np.ndarray] = None,
-        poses_se3: typing.Optional[typing.Sequence[np.ndarray]] = None,
-        meta: typing.Optional[dict] = None):
+            self, positions_xyz: typing.Optional[np.ndarray] = None,
+            orientations_quat_wxyz: typing.Optional[np.ndarray] = None,
+            timestamps: typing.Optional[np.ndarray] = None,
+            poses_se3: typing.Optional[typing.Sequence[np.ndarray]] = None,
+            meta: typing.Optional[dict] = None):
         """
         :param timestamps: optional nx1 list of timestamps
         """

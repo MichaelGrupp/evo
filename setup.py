721aa677e68f0de6ca7bf90bbd1962002869aa75
Michael Grupp <grupp@magazino.eu>
2022-01-03 15:53:19 +0100

    Follow Python version requirements of rosbags

--- setup.py
+++ setup.py
@@ -111,10 +111,9 @@ setup(
         "Topic :: Scientific/Engineering",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )

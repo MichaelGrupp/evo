4e2c7aaf3596564ef20c1effe166fecd769aca07
Michael Grupp <grupp@magazino.eu>
2023-07-12 15:58:00 +0200

    Add Python 3.11 to setup.py

--- setup.py
+++ setup.py
@@ -115,6 +115,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )

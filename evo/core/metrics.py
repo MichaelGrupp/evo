MichaelGrupp/evo

commit ca06efe45056b4e4f6c981816451c951c6e2cf01

Allow to change the unit of metrics. (#569)
Allows for example to see results in centimeters instead of meters.
Available in evo_ape and evo_rpe with `--change_unit`.

Includes an improved naming of angular pose relations to remove double
mention of the unit.

Implements feature request from issue #307.

evo/core/metrics.py
@@ -20,6 +20,7 @@ along with evo.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import abc
+import itertools
 import logging
 import math
 import sys
@@ -61,15 +62,18 @@ class PoseRelation(Enum):
     full_transformation = "full transformation"
     translation_part = "translation part"
     rotation_part = "rotation part"
-    rotation_angle_rad = "rotation angle in radians"
-    rotation_angle_deg = "rotation angle in degrees"
+    rotation_angle_rad = "rotation angle"
+    rotation_angle_deg = "rotation angle"
     point_distance = "point distance"
     point_distance_error_ratio = "point distance error ratio"
 
 
 class Unit(Enum):
     none = "unit-less"
+    millimeters = "mm"
+    centimeters = "cm"
     meters = "m"
+    kilometers = "km"
     seconds = "s"
     degrees = "deg"
     radians = "rad"
@@ -81,6 +85,19 @@ class VelUnit(Enum):
     meters_per_sec = "m/s"
     rad_per_sec = "rad/s"
     degrees_per_sec = "deg/s"
+
+
+LENGTH_UNITS = (Unit.millimeters, Unit.centimeters, Unit.meters,
+                Unit.kilometers)
+ANGLE_UNITS = (Unit.degrees, Unit.radians)
+
+# Factors to apply to a value a to convert it to meters.
+METER_SCALE_FACTORS = {
+    Unit.millimeters: 1e-3,
+    Unit.centimeters: 1e-2,
+    Unit.meters: 1,
+    Unit.kilometers: 1e3
+}
 
 
 class Metric(ABC):
@@ -106,8 +123,8 @@ class PE(Metric):
     Abstract base class of pose error metrics.
     """
     def __init__(self):
-        self.unit = Unit.none
-        self.error = np.array([])
+        self.unit: Unit = Unit.none
+        self.error: np.ndarray = np.array([])
 
     def __str__(self) -> str:
         return "PE metric base class"
@@ -115,6 +132,36 @@ class PE(Metric):
     @abc.abstractmethod
     def process_data(self, data):
         return
+
+    def change_unit(self, new_unit: Unit) -> None:
+        if self.unit is new_unit:
+            return
+
+        if self.unit in (Unit.none, Unit.frames, Unit.percent, Unit.seconds):
+            raise MetricsException(f"{self.unit} does not support conversions")
+
+        bad_combinations = list(itertools.product(ANGLE_UNITS, LENGTH_UNITS))
+        if any(combi in bad_combinations
+               for combi in itertools.permutations((self.unit, new_unit))):
+            raise MetricsException(f"cannot convert {self.unit} to {new_unit}")
+
+        if len(self.error) == 0:
+            raise MetricsException(
+                "error array is empty - "
+                "please process data before changing the unit")
+
+        if self.unit in LENGTH_UNITS and new_unit in LENGTH_UNITS:
+            # Convert first to meters, then to the final length unit.
+            self.error *= (METER_SCALE_FACTORS[self.unit] /
+                           METER_SCALE_FACTORS[new_unit])
+        elif self.unit is Unit.radians and new_unit is Unit.degrees:
+            self.error = np.rad2deg(self.error)
+        elif self.unit is Unit.degrees and new_unit is Unit.radians:
+            self.error = np.deg2rad(self.error)
+        else:
+            raise MetricsException(
+                f"unknown unit combination {(self.unit, new_unit)}")
+        self.unit = new_unit
 
     def get_statistic(self, statistics_type: StatisticsType) -> float:
         if statistics_type == StatisticsType.rmse:
@@ -305,7 +352,8 @@ class RPE(PE):
             # Already computed, see above.
             pass
         elif self.pose_relation == PoseRelation.translation_part:
-            self.error = [np.linalg.norm(E_i[:3, 3]) for E_i in self.E]
+            self.error = np.array(
+                [np.linalg.norm(E_i[:3, 3]) for E_i in self.E])
         elif self.pose_relation == PoseRelation.rotation_part:
             # ideal: rot(E_i) = 3x3 identity
             self.error = np.array([

evo/main_ape.py
@@ -23,6 +23,7 @@ along with evo.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import logging
+import typing
 
 import numpy as np
 
@@ -66,6 +67,10 @@ def parser() -> argparse.ArgumentParser:
         "--align_origin",
         help="align the trajectory origin to the origin of the reference "
         "trajectory", action="store_true")
+    algo_opts.add_argument(
+        "--change_unit", default=None, choices=[
+            u.value for u in (metrics.ANGLE_UNITS + metrics.LENGTH_UNITS)
+        ], help="Changes the output unit of the metric, if possible.")
 
     output_opts.add_argument(
         "-p",
@@ -190,7 +195,8 @@ def ape(traj_ref: PosePath3D, traj_est: PosePath3D,
         pose_relation: metrics.PoseRelation, align: bool = False,
         correct_scale: bool = False, n_to_align: int = -1,
         align_origin: bool = False, ref_name: str = "reference",
-        est_name: str = "estimate") -> Result:
+        est_name: str = "estimate",
+        change_unit: typing.Optional[metrics.Unit] = None) -> Result:
 
     # Align the trajectories.
     only_scale = correct_scale and not align
@@ -208,6 +214,9 @@ def ape(traj_ref: PosePath3D, traj_est: PosePath3D,
     data = (traj_ref, traj_est)
     ape_metric = metrics.APE(pose_relation)
     ape_metric.process_data(data)
+
+    if change_unit:
+        ape_metric.change_unit(change_unit)
 
     title = str(ape_metric)
     if align and not correct_scale:
@@ -277,18 +286,13 @@ def run(args: argparse.Namespace) -> None:
             first_name=ref_name, snd_name=est_name)
 
     pose_relation = common.get_pose_relation(args)
+    change_unit = metrics.Unit(args.change_unit) if args.change_unit else None
 
-    result = ape(
-        traj_ref=traj_ref,
-        traj_est=traj_est,
-        pose_relation=pose_relation,
-        align=args.align,
-        correct_scale=args.correct_scale,
-        n_to_align=args.n_to_align,
-        align_origin=args.align_origin,
-        ref_name=ref_name,
-        est_name=est_name,
-    )
+    result = ape(traj_ref=traj_ref, traj_est=traj_est,
+                 pose_relation=pose_relation, align=args.align,
+                 correct_scale=args.correct_scale, n_to_align=args.n_to_align,
+                 align_origin=args.align_origin, ref_name=ref_name,
+                 est_name=est_name, change_unit=change_unit)
 
     if args.plot or args.save_plot or args.serialize_plot:
         common.plot_result(args, result, traj_ref,

evo/main_rpe.py
@@ -23,6 +23,7 @@ along with evo.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import logging
+import typing
 
 import numpy as np
 
@@ -82,6 +83,10 @@ def parser() -> argparse.ArgumentParser:
     algo_opts.add_argument(
         "--pairs_from_reference", action="store_true",
         help="determine the pose pairs from the reference trajectory")
+    algo_opts.add_argument(
+        "--change_unit", default=None, choices=[
+            u.value for u in (metrics.ANGLE_UNITS + metrics.LENGTH_UNITS)
+        ], help="Changes the output unit of the metric, if possible.")
 
     output_opts.add_argument(
         "-p",
@@ -208,7 +213,8 @@ def rpe(traj_ref: PosePath3D, traj_est: PosePath3D,
         all_pairs: bool = False, pairs_from_reference: bool = False,
         align: bool = False, correct_scale: bool = False, n_to_align: int = -1,
         align_origin: bool = False, ref_name: str = "reference",
-        est_name: str = "estimate", support_loop: bool = False) -> Result:
+        est_name: str = "estimate", support_loop: bool = False,
+        change_unit: typing.Optional[metrics.Unit] = None) -> Result:
 
     # Align the trajectories.
     only_scale = correct_scale and not align
@@ -227,6 +233,9 @@ def rpe(traj_ref: PosePath3D, traj_est: PosePath3D,
     rpe_metric = metrics.RPE(pose_relation, delta, delta_unit, rel_delta_tol,
                              all_pairs, pairs_from_reference)
     rpe_metric.process_data(data)
+
+    if change_unit:
+        rpe_metric.change_unit(change_unit)
 
     title = str(rpe_metric)
     if align and not correct_scale:
@@ -293,6 +302,7 @@ def run(args: argparse.Namespace) -> None:
     traj_ref, traj_est, ref_name, est_name = common.load_trajectories(args)
     pose_relation = common.get_pose_relation(args)
     delta_unit = common.get_delta_unit(args)
+    change_unit = metrics.Unit(args.change_unit) if args.change_unit else None
 
     traj_ref_full = None
     if args.plot_full_ref:
@@ -313,22 +323,14 @@ def run(args: argparse.Namespace) -> None:
             traj_ref, traj_est, args.t_max_diff, args.t_offset,
             first_name=ref_name, snd_name=est_name)
 
-    result = rpe(
-        traj_ref=traj_ref,
-        traj_est=traj_est,
-        pose_relation=pose_relation,
-        delta=args.delta,
-        delta_unit=delta_unit,
-        rel_delta_tol=args.delta_tol,
-        all_pairs=args.all_pairs,
-        pairs_from_reference=args.pairs_from_reference,
-        align=args.align,
-        correct_scale=args.correct_scale,
-        n_to_align=args.n_to_align,
-        align_origin=args.align_origin,
-        ref_name=ref_name,
-        est_name=est_name,
-    )
+    result = rpe(traj_ref=traj_ref, traj_est=traj_est,
+                 pose_relation=pose_relation, delta=args.delta,
+                 delta_unit=delta_unit, rel_delta_tol=args.delta_tol,
+                 all_pairs=args.all_pairs,
+                 pairs_from_reference=args.pairs_from_reference,
+                 align=args.align, correct_scale=args.correct_scale,
+                 n_to_align=args.n_to_align, align_origin=args.align_origin,
+                 ref_name=ref_name, est_name=est_name, change_unit=change_unit)
 
     if args.plot or args.save_plot or args.serialize_plot:
         common.plot_result(args, result, traj_ref,

commit 6c27ad700dd7f7392370ce8299297ea5096e42c2

Fix pose relation enum (#581)
Fixes a bug introduced recently in PR #569 and switches all enums to @unique
to catch this in the future.

Fixes: #579

evo/core/metrics.py
@@ -25,7 +25,7 @@ import logging
 import math
 import sys
 import typing
-from enum import Enum
+from enum import Enum, unique
 
 import numpy as np
 
@@ -50,6 +50,7 @@ class MetricsException(EvoException):
     pass
 
 
+@unique
 class StatisticsType(Enum):
     rmse = "rmse"
     mean = "mean"
@@ -60,12 +61,13 @@ class StatisticsType(Enum):
     sse = "sse"
 
 
+@unique
 class PoseRelation(Enum):
     full_transformation = "full transformation"
     translation_part = "translation part"
     rotation_part = "rotation part"
-    rotation_angle_rad = "rotation angle"
-    rotation_angle_deg = "rotation angle"
+    rotation_angle_rad = "rotation angle in radians"
+    rotation_angle_deg = "rotation angle in degrees"
     point_distance = "point distance"
     point_distance_error_ratio = "point distance error ratio"
 

evo/core/units.py
@@ -19,9 +19,10 @@ You should have received a copy of the GNU General Public License
 along with evo.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from enum import Enum
+from enum import Enum, unique
 
 
+@unique
 class Unit(Enum):
     none = "unit-less"
     millimeters = "mm"

evo/tools/plot.py
@@ -26,7 +26,7 @@ import collections.abc
 import logging
 import pickle
 import typing
-from enum import Enum
+from enum import Enum, unique
 
 import matplotlib as mpl
 from evo.tools.settings import SETTINGS
@@ -71,6 +71,7 @@ class PlotException(EvoException):
     pass
 
 
+@unique
 class PlotMode(Enum):
     xy = "xy"
     xz = "xz"
@@ -81,6 +82,7 @@ class PlotMode(Enum):
     xyz = "xyz"
 
 
+@unique
 class Viewport(Enum):
     update = "update"
     keep_unchanged = "keep_unchanged"
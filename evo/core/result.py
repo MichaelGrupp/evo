Fix merged result average.

evo/core/result.py
@@ -113,15 +113,24 @@ def merge_results(results: typing.Sequence[Result]) -> Result:
     logger.warning("Using info dict of first result.")
     for result in results[1:]:
         merged_result.stats = {
-            key: ((merged_result.stats[key] + result.stats[key]) / 2)
+            key: merged_result.stats[key] + result.stats[key]
             for key in merged_result.stats
         }
         for key, array in merged_result.np_arrays.items():
             if strategy == "average":
-                merged_result.np_arrays[key] = np.mean(
-                    (array, result.np_arrays[key]), axis=0)
+                merged_result.np_arrays[key] = np.add(array,
+                                                      result.np_arrays[key])
             elif strategy == "append":
                 merged_result.np_arrays[key] = np.append(
                     array, result.np_arrays[key])
+
+    merged_result.stats = {
+        key: merged_result.stats[key] / len(results)
+        for key in merged_result.stats
+    }
+    if strategy == "average":
+        for key, array in merged_result.np_arrays.items():
+            merged_result.np_arrays[key] = np.divide(
+                merged_result.np_arrays[key], len(results))
 
     return merged_result

test/test_result.py
@@ -32,12 +32,15 @@ class TestMergeResult(unittest.TestCase):
         r1.add_np_array("test", np.array([1., 2., 3.]))
         r1.add_stats({"bla": 1., "blub": 2.})
         r2 = result.Result()
-        r2.add_np_array("test", np.array([0., 0., 0.]))
-        r2.add_stats({"bla": 0., "blub": 0.})
-        merged = result.merge_results([r1, r2])
+        r2.add_np_array("test", np.array([1., 0., 0.]))
+        r2.add_stats({"bla": 0., "blub": 1.})
+        r3 = result.Result()
+        r3.add_np_array("test", np.array([1., 1., 0.]))
+        r3.add_stats({"bla": 2., "blub": 0.})
+        merged = result.merge_results([r1, r2, r3])
         self.assertTrue(
-            np.array_equal(merged.np_arrays["test"], np.array([0.5, 1., 1.5])))
-        self.assertEqual(merged.stats, {"bla": 0.5, "blub": 1.})
+            np.array_equal(merged.np_arrays["test"], np.array([1., 1., 1.])))
+        self.assertEqual(merged.stats, {"bla": 1., "blub": 1.})
 
     def test_merge_strategy_append(self):
         r1 = result.Result()
@@ -45,14 +48,15 @@ class TestMergeResult(unittest.TestCase):
         r1.add_stats({"bla": 1., "blub": 2.})
         r2 = result.Result()
         r2.add_np_array("test", np.array([0.]))
-        r2.add_stats({"bla": 0., "blub": 0.})
-        merged = result.merge_results([r1, r2])
-        #yapf: disable
+        r2.add_stats({"bla": 0., "blub": 1.})
+        r3 = result.Result()
+        r3.add_np_array("test", np.array([1.]))
+        r3.add_stats({"bla": 2., "blub": 0.})
+        merged = result.merge_results([r1, r2, r3])
         self.assertTrue(
             np.array_equal(merged.np_arrays["test"],
-                           np.array([1., 2., 3., 0.])))
-        # yapf: enable
-        self.assertEqual(merged.stats, {"bla": 0.5, "blub": 1.})
+                           np.array([1., 2., 3., 0., 1.])))
+        self.assertEqual(merged.stats, {"bla": 1., "blub": 1.})
 
     def test_non_matching_np_arrays_keys(self):
         r1 = result.Result()
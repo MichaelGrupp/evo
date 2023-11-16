5cb5f78c8dae868b1648ec280685eaff8fd59b81
Michael Grupp <grupp@magazino.eu>
2023-11-16 15:51:30 +0100

    Test quaternion

--- test/test_trajectory.py
+++ test/test_trajectory.py
@@ -151,11 +151,24 @@ class TestPosePath3D(unittest.TestCase):
             angle = path.get_orientations_euler("sxyz")[0][null_dim]
 
             path.project(plane)
+            # Position value has to be zero outside of the plane.
             self.assertEqual(path.poses_se3[0][null_dim][3], 0)
             self.assertEqual(path.positions_xyz[0][null_dim], 0)
+            # Rotation has to be angle around the nulled dimension's axis.
             self.assertTrue(
                 np.allclose(lie.so3_log(lie.so3_from_se3(path.poses_se3[0])),
                             angle * axis))
+            # Quaternion has to be zero around the plane's basis vectors.
+            quat_wxyz = path.orientations_quat_wxyz[0]
+            if plane == trajectory.ProjectionPlane.xy:
+                self.assertAlmostEqual(quat_wxyz[1], 0)  # x
+                self.assertAlmostEqual(quat_wxyz[2], 0)  # y
+            elif plane == trajectory.ProjectionPlane.xz:
+                self.assertAlmostEqual(quat_wxyz[1], 0)  # x
+                self.assertAlmostEqual(quat_wxyz[3], 0)  # z
+            elif plane == trajectory.ProjectionPlane.yz:
+                self.assertAlmostEqual(quat_wxyz[2], 0)  # y
+                self.assertAlmostEqual(quat_wxyz[3], 0)  # z
 
 
 class TestPoseTrajectory3D(unittest.TestCase):

6308eb420f457d836c0ce4b4d15c8df47540f830
Michael Grupp <michael.grupp@tum.de>
2024-05-09 11:35:46 +0200

    revert test

--- evo/__init__.py
+++ evo/__init__.py
@@ -8,7 +8,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 PACKAGE_BASE_PATH = Path(__file__).absolute().parent
 
-__version__ = "v1.28.0"
+__version__ = "v1.27.2"
 
 
 class EvoException(Exception):

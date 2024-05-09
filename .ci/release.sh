dc818bfa5688babcd0fd2061c46cd8bb8c414d8f
Michael Grupp <michael.grupp@tum.de>
2024-05-09 12:41:34 +0200

    Do a hatch clean in release.sh

--- .ci/release.sh
+++ .ci/release.sh
@@ -15,17 +15,18 @@ if [ -n "$(git fetch --dry-run)" ]; then
     exit 1
 fi
 
+hatch version "$NEW_VERSION"
+
 PREVIOUS_VERSION="$(git describe --tags --abbrev=0)"
 echo "Current version: $PREVIOUS_VERSION"
 read -p "Enter new version: " NEW_VERSION
 read -p "Enter tag message: " TAG_MESSAGE
-
-hatch version "$NEW_VERSION"
 
 git commit -aem "Bump version to $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION: $TAG_MESSAGE"
 git push
 git push --tags
 
+hatch clean
 hatch build
 hatch publish

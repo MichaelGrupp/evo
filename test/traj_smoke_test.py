7565ffb38f219f3542c7f2913f33c1c333b42874
Michael Grupp <grupp@magazino.eu>
2024-06-13 14:53:02 +0200

    Smoke test for ROS 2 TF bags

--- test/traj_smoke_test.py
+++ test/traj_smoke_test.py
@@ -6,14 +6,14 @@ import shutil
 import subprocess as sp
 from pathlib import Path
 
-tmp_dir = Path("tmp")
-common_cfg_dir = Path("cfg/traj/common")
-here = Path(__file__).absolute().parent
+TMP_DIR = Path("tmp")
+COMMON_CONFIG_DIR = Path("cfg/traj/common")
+HERE = Path(__file__).absolute().parent
 
 # always run in script location
-os.chdir(here)
+os.chdir(HERE)
 
-data = {
+commands_with_config_dir = {
     "evo_traj euroc data/V102_groundtruth.csv --ref data/V102_groundtruth.csv": "cfg/traj/euroc",
     "evo_traj kitti data/KITTI_00_gt.txt data/KITTI_00_ORB.txt data/KITTI_00_SPTAM.txt "
     "--ref data/KITTI_00_gt.txt": "cfg/traj/kitti",
@@ -22,20 +22,23 @@ data = {
     "evo_traj bag data/ROS_example.bag groundtruth S-PTAM ORB-SLAM --ref groundtruth": "cfg/traj/bag"
 }
 
-if os.getenv("ROS_DISTRO", "") == "noetic":
-    data.update({
+if os.getenv("ROS_DISTRO") is not None:
+    # TF interface is able to load TF from ROS 1 bags in ROS 2 and vice versa.
+    commands_with_config_dir.update({
         "evo_traj bag data/tf_example.bag /tf:odom.base_link --ref /tf:odom.base_footprint": "cfg/traj/bag",
+        "evo_traj bag2 data/tf_example /tf:odom.base_link --ref /tf:odom.base_footprint": "cfg/traj/bag",
     })
 
 try:
-    for d in data.keys():
-        for cfg_dir in (common_cfg_dir, Path(data[d])):
-            for cfg in cfg_dir.iterdir():
-                tmp_dir.mkdir(exist_ok=True)
-                cmd = "{} -c {}".format(d, cfg)
-                print("[smoke test] {}".format(cmd))
-                output = sp.check_output(cmd.split(" "), cwd=here)
-                shutil.rmtree(tmp_dir)
+    for command in commands_with_config_dir.keys():
+        for config_dir in (COMMON_CONFIG_DIR,
+                           Path(commands_with_config_dir[command])):
+            for config_file in config_dir.iterdir():
+                TMP_DIR.mkdir(exist_ok=True)
+                full_command = f"{command} -c {config_file}"
+                print("[smoke test] {}".format(full_command))
+                output = sp.check_output(full_command.split(" "), cwd=HERE)
+                shutil.rmtree(TMP_DIR)
 except sp.CalledProcessError as e:
     print(e.output.decode("utf-8"))
     raise
@@ -44,5 +47,5 @@ finally:
         "./*.tum")
     for traj_file in traj_files:
         os.remove(traj_file)
-    if tmp_dir.exists():
-        shutil.rmtree(tmp_dir)
+    if TMP_DIR.exists():
+        shutil.rmtree(TMP_DIR)

7a32f62d61422bdcbc0c5cde73adacc769fa058b
TanPinDa <TanPinDa@gmail.com>
2024-06-13 14:41:20 +0000

    [#679] Bugfix: read_tum function called when reading euroc format

--- evo/common_ape_rpe.py
+++ evo/common_ape_rpe.py
@@ -51,7 +51,7 @@ def load_trajectories(
         ref_name, est_name = args.ref_file, args.est_file
     elif args.subcommand == "euroc":
         traj_ref = file_interface.read_euroc_csv_trajectory(args.state_gt_csv)
-        traj_est = file_interface.read_tum_trajectory_file(args.est_file)
+        traj_est = file_interface.read_euroc_csv_trajectory(args.est_file)
         ref_name, est_name = args.state_gt_csv, args.est_file
     elif args.subcommand in ("bag", "bag2"):
         logger.debug("Opening bag file " + args.bag)

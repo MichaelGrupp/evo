cc9ba392d66deb2c118cf224376468c0f8c5638b
Michael Grupp <grupp@magazino.eu>
2024-10-14 17:17:36 +0200

    Bump version to 1.30.1

--- evo/__init__.py
+++ evo/__init__.py
@@ -8,7 +8,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 PACKAGE_BASE_PATH = Path(__file__).absolute().parent
 
-__version__ = "v1.30.0"
+__version__ = "v1.30.1"
 
 
 class EvoException(Exception):

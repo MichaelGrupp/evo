6e7b5aeb22d8b96024bae9d2a4a308f7a11d52e5
Michael Grupp <michael@rerun.io>
2026-04-08 18:14:42 +0200

    Bump version to v1.35.2

--- evo/__init__.py
+++ evo/__init__.py
@@ -12,7 +12,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 PACKAGE_BASE_PATH = Path(__file__).absolute().parent
 
-__version__ = "v1.35.1"
+__version__ = "v1.35.2"
 
 
 class EvoException(Exception):
